652e6baf83d7a6ce1a7923c7ac3cdb1e23651eb6
jacobogonzalezdiaz1974 <jacobogonzalez1974@outlook.com>
2025-01-21 23:10:48 +0000

    initial commit

---
+++ client/src/app/dashboard/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { CheckCircle, Package, Tag, TrendingDown, TrendingUp } from "lucide-react";
+import CardExpenseSummary from "./CardExpenseSummary";
+import CardPopularProducts from "./CardPopularProducts";
+import CardPurchaseSummary from "./CardPurchaseSummary";
+import CardSalesSummary from "./CardSalesSummary";
+import StatCard from "./StatCard";
+
+const Dashboard = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <CardPopularProducts />
+      <CardSalesSummary />
+      <CardPurchaseSummary />
+      <CardExpenseSummary />
+      <StatCard
+        primaryIcon={<Package className="text-blue-600 w-6 h-6" />}
+        dateRange="22 - 29 October 2024"
+        title="Customer & Expenses"
+        details={[
+          {
+            title: "Customer Growth",
+            amount: "175.00",
+            changePercentage: 131,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Expenses",
+            amount: "10.00",
+            changePercentage: -56,
+            IconComponent: TrendingDown,
+          },
+        ]}
+      />
+      <StatCard
+        primaryIcon={<CheckCircle className="text-blue-600 w-6 h-6" />}
+        dateRange="22 - 29 October 2024"
+        title="Dues & Pending Orders"
+        details={[
+          {
+            title: "Dues",
+            amount: "250.00",
+            changePercentage: 131,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Pending Orders",
+            amount: "147",
+            changePercentage: -56,
+            IconComponent: TrendingDown,
+          },
+        ]}
+      />
+      <StatCard
+        primaryIcon={<Tag className="text-blue-600 w-6 h-6" />}
+        dateRange="22 - 29 October 2024"
+        title="Sales & Discount"
+        details={[
+          {
+            title: "Sales",
+            amount: "1000",
+            changePercentage: 20,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Discount",
+            amount: "200.00",
+            changePercentage: -10,
+            IconComponent: TrendingDown,
+          },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default Dashboard;
